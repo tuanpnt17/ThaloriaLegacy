3c8a6d3e4ad7b56ecbb26af7c03cccd295a5a112
zollie <thaonguyen07112004@gmail.com>
2025-03-20 16:47:31 +0700

    addskip in introduction and adjust bg

--- Assets/Scrips/TypingEffectManager.cs
+++ Assets/Scrips/TypingEffectManager.cs
@@ -1,78 +1,87 @@
+
 using System.Collections;
 using UnityEngine;
 using TMPro;
-using UnityEngine.SceneManagement; // ?? chuy?n scene sang Menu chính
+using UnityEngine.SceneManagement;
 
 public class TypingEffectManager : MonoBehaviour
 {
-    public TextMeshProUGUI dialogueText; // UI TextMeshPro ?? hi?n th? ch?
-    public float typingSpeed = 0.001f; // T?c ?? gõ ch?
-    public float waitTimeAfterText = 5.0f; // Th?i gian ch? sau khi hi?n th? xong m?i ?o?n
-    public CanvasGroup textCanvasGroup; // ?? t?o hi?u ?ng fade out
-    public AudioSource typingSound; 
+    public TextMeshProUGUI dialogueText;
+    public float typingSpeed = 0.05f;
+    public float waitTimeAfterText = 3.0f;
+    public CanvasGroup textCanvasGroup;
+    public AudioSource typingSound;
+    public GameObject skipButton;
 
-    // Danh sách các ?o?n v?n b?n s? hi?n th?
     private string[] storyTexts = new string[]
     {
-        "[Prologue] – The Awakening of Drakthor\r\nThe once-glorious kingdom of Thaloria now lies in ruins. ",
+        "[Prologue] – The Awakening of Drakthor\nThe once-glorious kingdom of Thaloria now lies in ruins.",
         "Drakthor the Eternal Dragon, an immortal beast, has awakened from Infernal Hollow, a cavern buried deep within a colossal volcano.",
-        "With a burning desire to reduce the world to ashes and create a new empire for dragons" ,
-        "He has summoned The Four Harbingers of Doom, ancient entities wielding catastrophic power.",
-
+        "With a burning desire to reduce the world to ashes and create a new empire for dragons.",
+        "He has summoned The Four Harbingers of Doom, ancient entities wielding catastrophic power."
     };
 
-    private int currentTextIndex = 0; // ??m s? th? t? ?o?n hi?n t?i
+    private int currentTextIndex = 0;
+    private Coroutine typingCoroutine;
+    private bool isTyping = false;
+    private bool isSkipping = false;
 
     void Start()
     {
-        StartCoroutine(ShowTextSequence()); // B?t ??u hi?u ?ng ch?y ch?
+        StartCoroutine(ShowTextSequence());
     }
 
     IEnumerator ShowTextSequence()
     {
         while (currentTextIndex < storyTexts.Length)
         {
-            yield return StartCoroutine(TypeText(storyTexts[currentTextIndex])); // Hi?n t?ng ký t?
-            yield return new WaitForSeconds(waitTimeAfterText); // ??i m?t lúc sau khi hi?n xong ?o?n
-            yield return StartCoroutine(FadeOutText()); // Ch? bi?n m?t
+            isSkipping = false;
+            typingCoroutine = StartCoroutine(TypeText(storyTexts[currentTextIndex]));
+            yield return typingCoroutine;
+
+            if (!isSkipping) // N?u ch?a Skip, ??i tr??c khi fade out
+            {
+                yield return new WaitForSeconds(waitTimeAfterText);
+            }
+
+            yield return StartCoroutine(FadeOutText());
 
             currentTextIndex++; // Chuy?n sang ?o?n ti?p theo
         }
 
-        LoadMainMenu(); // Khi hoàn thành t?t c? ?o?n, vào menu chính
+        LoadMainMenu();
     }
 
     IEnumerator TypeText(string fullText)
     {
-        dialogueText.text = ""; // Xóa n?i dung c?
-        textCanvasGroup.alpha = 1; // ??m b?o ch? hi?n th?
-        if (typingSound != null)
+        dialogueText.text = "";
+        textCanvasGroup.alpha = 1;
+        isTyping = true;
+
+        if (typingSound != null && !typingSound.isPlaying)
         {
-            typingSound.loop = false;  // Không cho l?p vô h?n
             typingSound.Play();
         }
-        for (int i = 0; i < fullText.Length; i++)
+
+        foreach (char letter in fullText.ToCharArray())
         {
-            dialogueText.text += fullText[i]; // Hi?n th? t?ng ký t?
-            //if (typingSound != null && !typingSound.isPlaying) // Phát âm thanh n?u không b? trùng
-            //{
-                
-            //}
-            if (typingSound != null && !typingSound.isPlaying)
+            if (isSkipping) // N?u b?m Skip, hi?n th? toàn b? ?o?n ngay l?p t?c
             {
-                typingSound.Play();
+                dialogueText.text = fullText;
+                break;
             }
+
+            dialogueText.text += letter;
             yield return new WaitForSeconds(typingSpeed);
         }
-        if (typingSound != null)
-        {
-            typingSound.Stop();
-        }
+
+        isTyping = false;
+        typingSound?.Stop();
     }
 
     IEnumerator FadeOutText()
     {
-        float fadeDuration = 1.5f; // Th?i gian m? d?n
+        float fadeDuration = 1.5f;
         float elapsedTime = 0;
 
         while (elapsedTime < fadeDuration)
@@ -82,11 +91,37 @@ public class TypingEffectManager : MonoBehaviour
             yield return null;
         }
 
-        textCanvasGroup.alpha = 0; // Hoàn toàn bi?n m?t
+        textCanvasGroup.alpha = 0;
+    }
+
+    public void SkipToNextText()
+    {
+        isSkipping = true; // ?ánh d?u tr?ng thái Skip
+
+        if (isTyping) // N?u ch? ?ang ch?y, hi?n th? toàn b? ngay l?p t?c
+        {
+            StopCoroutine(typingCoroutine);
+            dialogueText.text = storyTexts[currentTextIndex];
+            isTyping = false;
+            typingSound?.Stop();
+        }
+        else // N?u ?ã hi?n th? xong, chuy?n ?o?n ngay l?p t?c
+        {
+            if (currentTextIndex < storyTexts.Length - 1)
+            {
+                currentTextIndex++;
+                StopAllCoroutines();
+                StartCoroutine(ShowTextSequence());
+            }
+            else
+            {
+                LoadMainMenu();
+            }
+        }
     }
 
     void LoadMainMenu()
     {
-        SceneManager.LoadScene("MenuScene"); // Thay "MainMenu" b?ng tên scene menu c?a b?n
+        SceneManager.LoadScene("MenuScene");
     }
 }
