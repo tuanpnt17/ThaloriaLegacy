bfbc9d129f7688c199b6a7f3acffe9668df883b8
TuanPham <tuanpntse173039@fpt.edu.vn>
2025-03-21 09:08:59 +0700

    Final update

--- Assets/Scrips/AudioManager.cs
+++ Assets/Scrips/AudioManager.cs
@@ -1,238 +1,319 @@
-using UnityEditor.U2D.Aseprite;
 using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
-	[SerializeField] private AudioSource effectAudioSource;
-	[SerializeField] private AudioSource defaultAudioSource;
-	[SerializeField] private AudioSource runningAudioSource;
-	[SerializeField] private AudioSource swordAudioSource;
-	[SerializeField] private AudioSource powerBallAudioSource;
-	[SerializeField] private AudioSource playerHurtAudioSource;
-	[SerializeField] private AudioSource bossAudioSource;
-	[SerializeField] private AudioSource enemyAudioSource;
-	[SerializeField] private AudioClip shootClip;
-	[SerializeField] private AudioClip reLoadClip;
-	[SerializeField] private AudioClip enegyClip;
-	[SerializeField] private AudioClip mainMenuClip;
-	[SerializeField] private AudioClip map1Clip;
-	[SerializeField] private AudioClip map2Clip;
-	[SerializeField] private AudioClip map3Clip;
-	[SerializeField] private AudioClip map4Clip;
-	[SerializeField] private AudioClip mapEndClip;
-	[SerializeField] private AudioClip runningClip;
-	[SerializeField] private AudioClip swordClip;
-	[SerializeField] private AudioClip explosionClip;
-	[SerializeField] private AudioClip playerDeathClip;
-	[SerializeField] private AudioClip gameOverClip;
-	[SerializeField] private AudioClip enemyDeathClip;
-	[SerializeField] private AudioClip enemyClip1;
-	[SerializeField] private AudioClip enemyClip2;
-	[SerializeField] private AudioClip enemyClip3;
-	[SerializeField] private AudioClip enemyWizardClip;
-	[SerializeField] private AudioClip enemyArcherClip;
-	[SerializeField] private AudioClip enemySlashClip;
-	[SerializeField] private AudioClip enemyAxeClip;
-	[SerializeField] private AudioClip enemyExplosionClip;
-	[SerializeField] private AudioClip dragonBossClip;
-	[SerializeField] private AudioClip dragonBossAttackClip;
-	[SerializeField] private AudioClip roboticBossClip;
-	[SerializeField] private AudioClip golemBossClip;
-	[SerializeField] private AudioClip pharaohBossClip;
-	[SerializeField] private AudioClip pharaohBossAttackClip;
-	[SerializeField] private AudioClip frostBossClip;
-	[SerializeField] private AudioClip frostBossAttackClip;
+    [SerializeField]
+    private AudioSource effectAudioSource;
 
+    [SerializeField]
+    private AudioSource defaultAudioSource;
 
-	public bool isRunning = false;  // Track running state
+    [SerializeField]
+    private AudioSource runningAudioSource;
 
-	public void PlayShootSound()
-	{
-		effectAudioSource.PlayOneShot(shootClip);
-	}
-	public void PlayReLoadSound()
-	{
-		effectAudioSource.PlayOneShot(reLoadClip);
-	}
-	public void PlayEnergySound()
-	{
-		effectAudioSource.PlayOneShot(enegyClip);
-	}
-	public void PlayRunningAudio()
-	{
-		if (!isRunning)
-		{
-			isRunning = true;
-			runningAudioSource.clip = runningClip;
-			runningAudioSource.Play();
-		}
-	}
+    [SerializeField]
+    private AudioSource swordAudioSource;
 
-	public void StopRunningAudio()
-	{
-		if (isRunning)
-		{
-			isRunning = false;
-			runningAudioSource.Stop();
-		}
-	}
-	public void PlaySwordSound()
-	{
-		swordAudioSource.PlayOneShot(swordClip);
-	}
+    [SerializeField]
+    private AudioSource powerBallAudioSource;
 
-	public void PlayPowerBallSound()
-	{
-		powerBallAudioSource.PlayOneShot(explosionClip);
-	}
+    [SerializeField]
+    private AudioSource playerHurtAudioSource;
 
+    [SerializeField]
+    private AudioSource bossAudioSource;
 
-	public void PlayPlayerDeathSound()
-	{
-		playerHurtAudioSource.PlayOneShot(playerDeathClip);
-	}
+    [SerializeField]
+    private AudioSource enemyAudioSource;
 
+    [SerializeField]
+    private AudioClip shootClip;
 
-	public void PlayEnemySound()
-	{
-		int randomIndex = Random.Range(1, 3); // Generate a random number between 0 and 2
-		AudioClip selectedClip = null;
+    [SerializeField]
+    private AudioClip reLoadClip;
 
-		switch (randomIndex)
-		{
-			case 1:
-				selectedClip = enemyClip1;
-				break;
-			case 2:
-				selectedClip = enemyClip2;
-				break;
-			case 3:
-				selectedClip = enemyClip3;
-				break;
-		}
+    [SerializeField]
+    private AudioClip enegyClip;
 
-		if (selectedClip != null)
-		{
-			enemyAudioSource.PlayOneShot(selectedClip);
-		}
-	}
+    [SerializeField]
+    private AudioClip mainMenuClip;
 
-	public void PlayEnemyWizardSound()
-	{
-		enemyAudioSource.PlayOneShot(enemyWizardClip);
-	}
-	public void PlayEnemyArcherSound()
-	{
-		enemyAudioSource.PlayOneShot(enemyArcherClip);
-	}
+    [SerializeField]
+    private AudioClip map1Clip;
 
-	public void PlayEnemySlashSound()
-	{
-		enemyAudioSource.PlayOneShot(enemySlashClip);
-	}
+    [SerializeField]
+    private AudioClip map2Clip;
 
-	public void PlayEnemyAxeSound()
-	{
-		enemyAudioSource.PlayOneShot(enemyAxeClip);
-	}
-	public void PlayEnemyExplosionSound()
-	{
-		enemyAudioSource.PlayOneShot(enemyExplosionClip);
-	}
+    [SerializeField]
+    private AudioClip map3Clip;
 
-	public void PlayEnemyDeathSound()
-	{
-		enemyAudioSource.PlayOneShot(enemyDeathClip);
-	}
+    [SerializeField]
+    private AudioClip map4Clip;
 
-	public void PlayDragonBossAudio()
-	{
-		bossAudioSource.clip = dragonBossClip;
-		bossAudioSource.Play();
-	}
+    [SerializeField]
+    private AudioClip mapEndClip;
 
-	public void PlayDragonBossAttackSound()
-	{
-		enemyAudioSource.PlayOneShot(dragonBossAttackClip);
-	}
+    [SerializeField]
+    private AudioClip runningClip;
 
-	public void PlayRoboticBossAudio()
-	{
-		bossAudioSource.clip = roboticBossClip;
-		bossAudioSource.Play();
-	}
-	public void PlayGolemBossAudio()
-	{
-		bossAudioSource.clip = golemBossClip;
-		bossAudioSource.Play();
-	}
+    [SerializeField]
+    private AudioClip swordClip;
 
-	public void PlayPharaohBossAudio()
-	{
-		bossAudioSource.clip = pharaohBossClip;
-		bossAudioSource.Play();
-	}
+    [SerializeField]
+    private AudioClip explosionClip;
 
-	public void PlayPharaohBossAttackSound()
-	{
-		enemyAudioSource.PlayOneShot(pharaohBossAttackClip);
-	}
+    [SerializeField]
+    private AudioClip playerDeathClip;
 
-	public void PlayFrostBossAudio()
-	{
-		bossAudioSource.clip = frostBossClip;
-		bossAudioSource.Play();
-	}
+    [SerializeField]
+    private AudioClip gameOverClip;
 
-	public void PlayFrostBossAttackSound()
-	{
-		enemyAudioSource.PlayOneShot(frostBossAttackClip);
-	}
+    [SerializeField]
+    private AudioClip enemyDeathClip;
 
-	public void PlayMainMenuAudio()
-	{
-	}
+    [SerializeField]
+    private AudioClip enemyClip1;
 
-	public void PlayMap1Audio()
-	{
-		defaultAudioSource.clip = map1Clip;
-		defaultAudioSource.Play();
-	}
-	public void PlayMap2Audio()
-	{
-		defaultAudioSource.clip = map2Clip;
-		defaultAudioSource.Play();
-	}
-	public void PlayMap3Audio()
-	{
-		defaultAudioSource.clip = map3Clip;
-		defaultAudioSource.Play();
-	}
-	public void PlayMap4Audio()
-	{
-		defaultAudioSource.clip = map4Clip;
-		defaultAudioSource.Play();
-	}
-	public void PlayMapEndAudio()
-	{
-		defaultAudioSource.clip = mapEndClip;
-		defaultAudioSource.Play();
-	}
+    [SerializeField]
+    private AudioClip enemyClip2;
 
-	public void StopAudioGame()
-	{
-		defaultAudioSource.Stop();
-		effectAudioSource.Stop();
-	}
+    [SerializeField]
+    private AudioClip enemyClip3;
 
-	public void StopBossAudio()
-	{
-		bossAudioSource.Stop();
-	}
+    [SerializeField]
+    private AudioClip enemyWizardClip;
 
-	public void PlayGameOverSound()
-	{
-		defaultAudioSource.PlayOneShot(gameOverClip);
-	}
+    [SerializeField]
+    private AudioClip enemyArcherClip;
 
+    [SerializeField]
+    private AudioClip enemySlashClip;
+
+    [SerializeField]
+    private AudioClip enemyAxeClip;
+
+    [SerializeField]
+    private AudioClip enemyExplosionClip;
+
+    [SerializeField]
+    private AudioClip dragonBossClip;
+
+    [SerializeField]
+    private AudioClip dragonBossAttackClip;
+
+    [SerializeField]
+    private AudioClip roboticBossClip;
+
+    [SerializeField]
+    private AudioClip golemBossClip;
+
+    [SerializeField]
+    private AudioClip pharaohBossClip;
+
+    [SerializeField]
+    private AudioClip pharaohBossAttackClip;
+
+    [SerializeField]
+    private AudioClip frostBossClip;
+
+    [SerializeField]
+    private AudioClip frostBossAttackClip;
+
+    public bool isRunning = false; // Track running state
+
+    public void PlayShootSound()
+    {
+        effectAudioSource.PlayOneShot(shootClip);
+    }
+
+    public void PlayReLoadSound()
+    {
+        effectAudioSource.PlayOneShot(reLoadClip);
+    }
+
+    public void PlayEnergySound()
+    {
+        effectAudioSource.PlayOneShot(enegyClip);
+    }
+
+    public void PlayRunningAudio()
+    {
+        if (!isRunning)
+        {
+            isRunning = true;
+            runningAudioSource.clip = runningClip;
+            runningAudioSource.Play();
+        }
+    }
+
+    public void StopRunningAudio()
+    {
+        if (isRunning)
+        {
+            isRunning = false;
+            runningAudioSource.Stop();
+        }
+    }
+
+    public void PlaySwordSound()
+    {
+        swordAudioSource.PlayOneShot(swordClip);
+    }
+
+    public void PlayPowerBallSound()
+    {
+        powerBallAudioSource.PlayOneShot(explosionClip);
+    }
+
+    public void PlayPlayerDeathSound()
+    {
+        playerHurtAudioSource.PlayOneShot(playerDeathClip);
+    }
+
+    public void PlayEnemySound()
+    {
+        int randomIndex = Random.Range(1, 3); // Generate a random number between 0 and 2
+        AudioClip selectedClip = null;
+
+        switch (randomIndex)
+        {
+            case 1:
+                selectedClip = enemyClip1;
+                break;
+            case 2:
+                selectedClip = enemyClip2;
+                break;
+            case 3:
+                selectedClip = enemyClip3;
+                break;
+        }
+
+        if (selectedClip != null)
+        {
+            enemyAudioSource.PlayOneShot(selectedClip);
+        }
+    }
+
+    public void PlayEnemyWizardSound()
+    {
+        enemyAudioSource.PlayOneShot(enemyWizardClip);
+    }
+
+    public void PlayEnemyArcherSound()
+    {
+        enemyAudioSource.PlayOneShot(enemyArcherClip);
+    }
+
+    public void PlayEnemySlashSound()
+    {
+        enemyAudioSource.PlayOneShot(enemySlashClip);
+    }
+
+    public void PlayEnemyAxeSound()
+    {
+        enemyAudioSource.PlayOneShot(enemyAxeClip);
+    }
+
+    public void PlayEnemyExplosionSound()
+    {
+        enemyAudioSource.PlayOneShot(enemyExplosionClip);
+    }
+
+    public void PlayEnemyDeathSound()
+    {
+        enemyAudioSource.PlayOneShot(enemyDeathClip);
+    }
+
+    public void PlayDragonBossAudio()
+    {
+        bossAudioSource.clip = dragonBossClip;
+        bossAudioSource.Play();
+    }
+
+    public void PlayDragonBossAttackSound()
+    {
+        enemyAudioSource.PlayOneShot(dragonBossAttackClip);
+    }
+
+    public void PlayRoboticBossAudio()
+    {
+        bossAudioSource.clip = roboticBossClip;
+        bossAudioSource.Play();
+    }
+
+    public void PlayGolemBossAudio()
+    {
+        bossAudioSource.clip = golemBossClip;
+        bossAudioSource.Play();
+    }
+
+    public void PlayPharaohBossAudio()
+    {
+        bossAudioSource.clip = pharaohBossClip;
+        bossAudioSource.Play();
+    }
+
+    public void PlayPharaohBossAttackSound()
+    {
+        enemyAudioSource.PlayOneShot(pharaohBossAttackClip);
+    }
+
+    public void PlayFrostBossAudio()
+    {
+        bossAudioSource.clip = frostBossClip;
+        bossAudioSource.Play();
+    }
+
+    public void PlayFrostBossAttackSound()
+    {
+        enemyAudioSource.PlayOneShot(frostBossAttackClip);
+    }
+
+    public void PlayMainMenuAudio() { }
+
+    public void PlayMap1Audio()
+    {
+        defaultAudioSource.clip = map1Clip;
+        defaultAudioSource.Play();
+    }
+
+    public void PlayMap2Audio()
+    {
+        defaultAudioSource.clip = map2Clip;
+        defaultAudioSource.Play();
+    }
+
+    public void PlayMap3Audio()
+    {
+        defaultAudioSource.clip = map3Clip;
+        defaultAudioSource.Play();
+    }
+
+    public void PlayMap4Audio()
+    {
+        defaultAudioSource.clip = map4Clip;
+        defaultAudioSource.Play();
+    }
+
+    public void PlayMapEndAudio()
+    {
+        defaultAudioSource.clip = mapEndClip;
+        defaultAudioSource.Play();
+    }
+
+    public void StopAudioGame()
+    {
+        defaultAudioSource.Stop();
+        effectAudioSource.Stop();
+    }
+
+    public void StopBossAudio()
+    {
+        bossAudioSource.Stop();
+    }
+
+    public void PlayGameOverSound()
+    {
+        defaultAudioSource.PlayOneShot(gameOverClip);
+    }
 }
